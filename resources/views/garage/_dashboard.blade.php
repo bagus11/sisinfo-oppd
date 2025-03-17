1b6b1533de2affe28287067f7ba217a7721b1353
unknown <bagus.oetomo11@gmail.com>
2025-03-18 02:10:15 +0700

    update condition report

--- resources/views/garage/_dashboard.blade.php
+++ resources/views/garage/_dashboard.blade.php
@@ -295,7 +295,7 @@
 </style>
 <body>    
     @php
-        $satgas = DB::table('master_satgas')::find(auth()->user()->satgas);
+        $satgas = DB::table('master_satgas')::where('id',auth()->user()->satgas)->first();
     @endphp
     <div class="preloader">
         <img src="{{ asset('assets/images/logos/logo.png') }}" alt="loader" class="lds-ripple img-fluid" />
